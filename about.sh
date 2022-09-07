72c047656d7340b66960bf12d6553fce137fcec6
adamcrew <90532227+scriptadamcrew@users.noreply.github.com>
2022-09-07 12:41:21 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "# Script Auto Install VPS PREMIUM               #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian & ubuntu 9 & 10 Buster 64 bit      #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by joysmark                          #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# ALLAH SWT                                     #"
+echo -e "# GROU TELEGRAM @joysmark                       #"
+echo -e "# PM @trikjoysmart                              #"
+echo -e "# PREMIUM SCRIPT                                #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
